13f916806f07b0a54941e293da9a76adccf0ba11
armansomoy <armansamoy42@gmail.com>
2020-07-16 16:21:04 +0600

    Second Time

---
+++ leapyearchecker.js
@@ -0,0 +1,9 @@
+// Author: Arman Hossain Somoy
+
+function leapyear(year) {
+    if ((year % 100 != 0) && (year % 4 == 0) || (year % 400 == 0)) {
+        alert('This is Leap Year');
+    } else {
+        alert('This is not Leap Year');
+    }
+}
