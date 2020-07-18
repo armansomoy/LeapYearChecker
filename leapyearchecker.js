d6fbe795f2cee68602b9d9c0b3d29007223d974e
armansomoy <armansamoy42@gmail.com>
2020-07-18 20:10:30 +0600

    Done

--- leapyearchecker.js
+++ leapyearchecker.js
@@ -1,7 +1,7 @@
 // Author: Arman Hossain Somoy
 
 function leapyear(year) {
-    if ((year % 100 != 0) && (year % 4 == 0) || (year % 400 == 0)) {
+    if ((year % 100 != 0) & (year % 4 == 0) | (year % 400 == 0)) {
         alert('This is Leap Year');
     } else {
         alert('This is not Leap Year');
